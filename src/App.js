cea5c5753b5251803835c208b79512103b61d909
Chris Conner <chris.conner57@gmail.com>
2020-04-03 11:51:21 -0700

    Finished layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Search from './Search';
+import House from './House';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [property, setProperty] = useState({
+		properties: [],
+		loading: false
+	});
+
+	useEffect(property => {
+		setProperty({ ...property, loading: true });
+
+		fetch(
+			'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz17aul58b763_9hc6e'
+		)
+			.then(res => res.json())
+			.then(res => setProperty({ properties: res, loading: false }));
+	}, []);
+
+	return (
+		<div className='App'>
+			<header>
+				<h1>Premium Property Finder</h1>
+				<p>Bringing premium property right to your finger tips</p>
+			</header>
+			<Search />
+			<House loading={property.loading} properties={property.properties} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+	return (
+		<div className='search-bar'>
+			<div className='search-arrow'></div>
+			<input type='search' placeholder='Enter address here...' />
+		</div>
+	);
+};
+
+export default Search;
